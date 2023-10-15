31f9cac29b642bfa4ba9a19e091a1c4c7d679ac8
Astridgal <astridgal9@gmail.com>
2023-10-15 18:01:55 -0500

    creando JS

---
+++ index.js
@@ -0,0 +1,14 @@
+const contenedor = document.getElementById("CollectivitiesParty")
+const card1 = document.createElement("div")
+card1.classList.add("card")
+card1.innerHTML = `<img src="./Recursos_Amazing_Events_Task_1/food_fair.jpg" class="card-img-top" height="191" alt="...">
+<div class="card-body">
+  <h2 class="card-title d-flex justify-content-center >">Festival of the collectivities</h2>
+  <p class="card-text d-flex">Enjoy your favorite dishes from different countries in a unique event for the whole family</p>
+  
+  <div class="d-flex justify-content-around">
+    <h3> Price: $5</h3>
+  <a href="Details.html" class="btn BotonesDetails">Details</a>
+</div>
+</div>
+</div>`
