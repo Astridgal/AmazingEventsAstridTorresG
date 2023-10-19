46574ac0faa30e0813979e3a2c707f059b9a33ca
Astridgal <astridgal9@gmail.com>
2023-10-18 20:59:40 -0500

    modificaciones a js

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 
-
-const contenedor = document.getElementById("ContenedorCards")
 const events = data.events
+const contenedor = document.getElementById("ContenedorCards")
+
 
 for(let Cards of events){
-
-
 const card1 = document.createElement("div")
 card1.classList.add("card")
 card1.innerHTML = `<img src="${Cards.image}" class="card-img-top" height="191" alt="...">
 <div class="card-body gap-1 d-flex flex-column justify-content-center">
   <h2 class="card-title d-flex justify-content-center">${Cards.name}</h2>
   <p class="card-text d-flex">${Cards.description}</p>
-  
   <div class="d-flex justify-content-around">
     <h3> Price: $ ${Cards.price}</h3>
   <a href="Details.html" class="btn BotonesDetails">Details</a>
